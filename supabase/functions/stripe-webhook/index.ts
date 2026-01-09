a011f945c91dafe95c975550d4c4e5dde20b934a
Vinicius Machado <vinicius@vinicius.com>
2026-01-08 23:28:44 -0300

    fix: atualiza libs das edge functions para compatibilidade com Deno 2

--- supabase/functions/stripe-webhook/index.ts
+++ supabase/functions/stripe-webhook/index.ts
@@ -1,6 +1,6 @@
-import { serve } from "https://deno.land/std@0.168.0/http/server.ts"
-import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
-import Stripe from 'https://esm.sh/stripe@12.0.0?target=deno'
+import { serve } from "https://deno.land/std@0.192.0/http/server.ts"
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2.39.7'
+import Stripe from 'https://esm.sh/stripe@13.10.0?target=deno'
 
 const stripe = new Stripe(Deno.env.get('STRIPE_SECRET_KEY') || '', {
     httpClient: Stripe.createFetchHttpClient(),
